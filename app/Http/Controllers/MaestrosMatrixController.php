d567f871245250386143617ec6dff4dd9d5e4f14
MaironU <maironurielesan@gmail.com>
2022-06-21 23:22:23 -0500

    añadiendo funcionalidad para obtener la relacion del campo

--- app/Http/Controllers/MaestrosMatrixController.php
+++ app/Http/Controllers/MaestrosMatrixController.php
@@ -116,25 +116,44 @@ class MaestrosMatrixController extends Controller
         ]);
     }
 
+
+
     public function datos(Request $request)
     {
         $request->validate([
             "tabla" => "required|string"
         ]);
-
         // permisos
         $permisos = DB::table('root_000105')->where('Tabusu', Auth::user()->Codigo)->where('Tabtab', $request->tabla)->first(['Tabtab', 'Tabcvi', 'Tabcam', 'Tabpgr', 'Tabopc']);
-
         // detalle
         $tabla_consecutivo = explode('_', $request->tabla);
-        $detalle = DB::table('det_formulario')->where('medico', $tabla_consecutivo[0])->where('codigo', $tabla_consecutivo[1])->where('activo', 'A')->get(['medico', 'codigo', 'campo', 'descripcion', 'tipo', 'posicion', 'comentarios']);
-
-        // descripcion
+        $detalle = DB::table('det_formulario')->where('medico', $tabla_consecutivo[0])
+        ->where('codigo', $tabla_consecutivo[1])
+        ->where('activo', 'A')
+        ->get(['medico', 'codigo', 'campo', 'descripcion', 'tipo', 'posicion', 'comentarios']);
         $descripciones = DB::table('root_000030')->where('Dic_Usuario', $tabla_consecutivo[0])->where('Dic_Formulario', $tabla_consecutivo[1])->get(['Dic_Campo', 'Dic_Descripcion']);
-
-        // data
+        
         $data = DB::table($request->tabla)->paginate(10);
 
+        foreach($data as $elem){
+            $position = 0;
+            $position_data_start = 0;
+            foreach($elem as $key => $value){
+                if($position_data_start >= 3){
+                    $det = $this->searchDescripcion($detalle, $key);
+                    if($det){
+                        if($det->tipo == "18" || $det->tipo == "9"){
+                            $destructur = explode("-", $det->comentarios);
+                            $relation = $this->getRelation($destructur, $tabla_consecutivo[0], $value);    
+                            $elem->{$key} = $relation;       
+                        }
+                        $position ++;
+                    }
+                }
+                $position_data_start++;
+            }
+        }
+        
         return response()->json([
             'data' => [
                 'permisos' => $permisos,
@@ -147,6 +166,109 @@ class MaestrosMatrixController extends Controller
         ]);
     }
 
+    public function searchDescripcion($detalle, $key){
+        $search = $detalle->where('descripcion', $key)->first();
+        return $search;
+    }
+
+    public function getRelation($destructur, $tabla, $value){
+        $num_datos = count($destructur);
+        $campos = array_slice($destructur, 1);
+        $campos_dos = array_slice($destructur, 2);
+
+        if(intval($destructur[0]) + 2 == sizeof($destructur)){
+            $select_campos_tabla = DB::table('det_formulario')
+                                    ->where('medico', $tabla)
+                                    ->where('codigo', $destructur[1])
+                                    ->whereIn('campo', $campos)
+                                    ->get();
+        
+            if(count($select_campos_tabla)){
+                $r=0;
+                $ppal = $select_campos_tabla[0]->descripcion;
+                $campos_select = "";
+    
+                foreach($select_campos_tabla as $elem){
+                    $campos_select .= $elem->descripcion.",'-',";
+                }
+                $campos_select = substr ($campos_select, 0, strlen($campos_select) - 5);
+                $relation = DB::table($tabla."_".$destructur[1])
+                                ->select("$ppal as ppal", DB::raw("CONCAT($campos_select) as seleccionado"))
+                                ->where($ppal, $value)
+                                ->first();
+
+
+                return $relation ? $relation->seleccionado : "";
+            } 
+        }else{
+            $select_campos_tabla = DB::table('det_formulario')
+                                    ->where('medico', $destructur[1])
+                                    ->where('codigo', $destructur[2])
+                                    ->whereIn('campo', $campos_dos)
+                                    ->get();
+
+            if(count($select_campos_tabla)){
+                $r=0;
+                $ppal = $select_campos_tabla[0]->descripcion;
+                $campos_select = "";
+    
+                foreach($select_campos_tabla as $elem){
+                    $campos_select .= $elem->descripcion.",'-',";
+                }
+                $campos_select = substr ($campos_select, 0, strlen($campos_select) - 5);
+
+                $relation = DB::table($destructur[1]."_".$destructur[2])
+                                ->select("$ppal as ppal", DB::raw("CONCAT($campos_select) as seleccionado"))
+                                ->where($ppal, $value)
+                                ->first();
+                
+                return $relation ? $relation->seleccionado : "";
+            } 
+        }
+        return "";
+    }
+    
+    public function getKeys($destructur, $medico){
+        $keys = [];
+        if (intval($destructur[0]) + 2 == sizeof($destructur)) {
+            $columns = DB::table('det_formulario')->where('medico', $medico)
+                ->where('codigo', $destructur[1])
+                ->whereIn('campo', array_slice($destructur, 2))
+                ->get('descripcion');
+        }else{
+            $columns = DB::connection('mysql')->table('det_formulario')
+                ->where('medico', $destructur[1])
+                ->where('codigo', $destructur[2])
+                ->whereIn('campo', array_slice($destructur, 3))
+                ->get('descripcion');
+        }
+
+        foreach ($columns as $value) {
+            $keys[] = $value->descripcion;
+        }
+
+        return $keys;
+    }
+
+    public function getKeys2($destructur, $medico, $keys){
+        $key2 = [];
+        if (intval($destructur[0]) + 2 == sizeof($destructur)) {
+            $consult = DB::connection('mysql')->table($medico . '_' . $destructur[1])->groupBy($keys[0])->get($keys);
+        }else{
+            $consult = DB::connection('mysql')->table($destructur[1] . '_' . $destructur[2])->groupBy($keys[0])->get($keys);
+        }
+
+        foreach ($consult as $select) {
+            $key2[] = [
+                'key' => current($select),
+                'text' => implode('-', array_values((array)$select)),
+                'value' => current($select)
+            ];
+        }
+        return $key2;
+    }
+
+
     public function relaciones(Request $request)
     {
         $request->validate([
@@ -156,53 +278,10 @@ class MaestrosMatrixController extends Controller
         ]);
 
         $destructur = explode("-", $request->comentarios);
-        if (intval($destructur[0]) + 2 == sizeof($destructur)) {
-            //selecciono las columnas
-            $columns = DB::connection('mysql')->table('det_formulario')
-                ->where('medico', $request->medico)
-                ->where('codigo', $destructur[1])
-                ->whereIn('campo', array_slice($destructur, 2))
-                ->get('descripcion');
-
-            $keys = [];
-            foreach ($columns as $value) {
-                $keys[] = $value->descripcion;
-            }
-
-            $consult = DB::connection('mysql')->table($request->medico . '_' . $destructur[1])->groupBy($keys[0])->get($keys);
-            $key2 = [];
-            foreach ($consult as $select) {
-                $key2[] = [
-                    'key' => current($select),
-                    'text' => implode('-', array_values((array)$select)),
-                    'value' => current($select)
-                ];
-            }
-        } else {
-            $columns = DB::connection('mysql')->table('det_formulario')
-                ->where('medico', $destructur[1])
-                ->where('codigo', $destructur[2])
-                ->whereIn('campo', array_slice($destructur, 3))
-                ->get('descripcion');
-
-            $keys = [];
-
-            foreach ($columns as $value) {
-                $keys[] = $value->descripcion;
-            }
-
-            $consult = DB::connection('mysql')->table($destructur[1] . '_' . $destructur[2])->groupBy($keys[0])->get($keys);
-
-            $key2 = [];
-            foreach ($consult as $select) {
-                $key2[] = [
-                    'key' => current($select),
-                    'text' => implode('-', array_values((array)$select)),
-                    'value' => current($select)
-                ];
-            }
-
-        }
+        
+        $keys = $this->getKeys($destructur, $request->medico);
+        $key2 = $this->getKeys2($destructur, $request->medico, $keys);
+        
         return response()->json(['data' => $key2,
             'message' => 'Se encontro unos opciones',
             'res' => true]);
